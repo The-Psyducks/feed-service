f31111d276bf4ac5859dc780a228f69213d0f3fd
Martu <mlozano@fi.uba.ar>
2024-09-30 16:20:55 -0300

    privacy tests para word-search

--- server/tests/wordsearch_test.go
+++ server/tests/wordsearch_test.go
@@ -55,10 +55,9 @@ func TestWordsearch(t *testing.T) {
 	time := time.Now().Format(time.RFC3339)
 
 	skip := "0"
-	feed_type := "foryou"
 	limit := "6"
 
-	getFeed, _ := http.NewRequest("GET", "/twitsnap/word-search?words="+words_wanted+"&time="+time+"&skip="+skip+"&limit="+limit+"&feed_type="+feed_type+"&wanted_user_id="+"", nil)
+	getFeed, _ := http.NewRequest("GET", "/twitsnap/word-search?words="+words_wanted+"&time="+time+"&skip="+skip+"&limit="+limit, nil)
 	addAuthorization(getFeed, token)
 
 	feedRecorder := httptest.NewRecorder()
@@ -76,6 +75,123 @@ func TestWordsearch(t *testing.T) {
 	assert.Equal(t, 6, result.Pagination.Limit)
 	assert.Equal(t, 0, result.Pagination.Next_Offset)
 }
+
+
+
+func TestWordSearchNotFollowing(t *testing.T) {
+
+	log.Println("TestHashagSearchNotFollowing: only posts with all the tags wanted appear, and only public posts from not followed users")
+
+	db := connectToDatabase()
+	
+	r := router.CreateRouter(db)
+	
+	words_wanted := "apple pie pecan"
+
+	words_wanted_list := strings.Split(words_wanted, " ")
+
+	makeAndAssertPost(service.TEST_NOT_FOLLOWING_ID, "content" + words_wanted_list[2], []string{"tags", "tags1"}, false, r, t)
+
+	time.Sleep(1 * time.Second)
+
+	post2 := makeAndAssertPost(service.TEST_USER_TWO, "content2" + words_wanted_list[0], []string{"tags_wanted[0]", "tags_wanted[1]"}, true, r, t)
+
+	time.Sleep(1 * time.Second)
+
+	makeAndAssertPost(service.TEST_USER_THREE, "content3", []string{service.TEST_TAG_THREE, "tag6"}, true, r, t)
+
+	makeAndAssertPost(service.TEST_USER_THREE, "content4", []string{"tag7", "tag8"}, true, r, t)
+
+	token, err := auth.GenerateToken("1", "username", true)
+
+	assert.Equal(t, err, nil)
+
+	expectedPosts := []models.FrontPost{post2}
+
+	result := models.ReturnPaginatedPosts{}
+
+	time.Sleep(1 * time.Second)
+	time := time.Now().Format(time.RFC3339)
+
+	skip := "0"
+	limit := "6"
+
+
+	getFeed, _ := http.NewRequest("GET", "/twitsnap/word-search?words="+words_wanted+"&time="+time+"&skip="+skip+"&limit="+limit, nil)
+	addAuthorization(getFeed, token)
+
+	feedRecorder := httptest.NewRecorder()
+	r.ServeHTTP(feedRecorder, getFeed)
+
+	err_2 := json.Unmarshal(feedRecorder.Body.Bytes(), &result)
+
+	assert.Equal(t, err_2, nil)
+	assert.Equal(t, http.StatusOK, feedRecorder.Code)
+
+	compareOrderAsExpected(expectedPosts, result.Data, t)
+	assertOnlyPublicPostsForNotFollowing(result.Data, t)
+	assert.Equal(t, 6, result.Pagination.Limit)
+	assert.Equal(t, 0, result.Pagination.Next_Offset)
+}
+
+func TestWordSearchFollowing(t *testing.T) {
+
+	log.Println("TestHashtagSearchFollowing: only posts with all the tags wanted appear, with public and not public posts from followed users")
+
+	db := connectToDatabase()
+
+	r := router.CreateRouter(db)
+
+	words_wanted := "apple pie pecan"
+
+	words_wanted_list := strings.Split(words_wanted, " ")
+
+	post1 := makeAndAssertPost(service.TEST_USER_ONE, "content" + words_wanted_list[1], []string{"tags_wanted"," tags_wanted[1]"}, false, r, t)
+
+	time.Sleep(1 * time.Second)
+
+	post2 := makeAndAssertPost(service.TEST_USER_TWO, "content2" + words_wanted_list[0], []string{"tags_wanted[0]", "tags_wanted[1]"}, true, r, t)
+
+	time.Sleep(1 * time.Second)
+
+	_ = makeAndAssertPost(service.TEST_USER_THREE, "content3", []string{service.TEST_TAG_THREE, "tag6"}, true, r, t)
+
+	makeAndAssertPost(service.TEST_USER_THREE, "content4", []string{"tag7", "tag8"}, true, r, t)
+
+	token, err := auth.GenerateToken("1", "username", true)
+
+	assert.Equal(t, err, nil)
+
+	expectedPosts := []models.FrontPost{post2, post1}
+
+	result := models.ReturnPaginatedPosts{}
+
+	time.Sleep(1 * time.Second)
+	time := time.Now().Format(time.RFC3339)
+
+	skip := "0"
+	limit := "6"
+
+
+	getFeed, _ := http.NewRequest("GET",  "/twitsnap/word-search?words="+words_wanted+"&time="+time+"&skip="+skip+"&limit="+limit, nil)
+	addAuthorization(getFeed, token)
+
+	feedRecorder := httptest.NewRecorder()
+	r.ServeHTTP(feedRecorder, getFeed)
+
+	err_2 := json.Unmarshal(feedRecorder.Body.Bytes(), &result)
+
+	// log.Println(result)
+
+	assert.Equal(t, err_2, nil)
+	assert.Equal(t, http.StatusOK, feedRecorder.Code)
+
+	compareOrderAsExpected(expectedPosts, result.Data, t)
+	assertOnlyPublicPostsForNotFollowing(result.Data, t)
+	assert.Equal(t, 6, result.Pagination.Limit)
+	assert.Equal(t, 0, result.Pagination.Next_Offset)
+}
+
 
 
 func TestWordSearchNextOffset(t *testing.T) {
